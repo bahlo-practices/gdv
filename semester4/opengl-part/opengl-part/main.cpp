ab98f146152244326230bda872bbf051ebf82b6b
Arne Bahlo <hallo@arne.me>
2014-04-13 19:57:22 +0200

    Initial Commit

---
+++ semester4/opengl-part/opengl-part/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  opengl-part
+//
+//  Created by Arne Bahlo on 13.04.14.
+//  Copyright (c) 2014 Arne Bahlo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
