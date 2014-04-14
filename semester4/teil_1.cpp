d9de61e07e83a31c8524aea2dd94d8391db5dfc3
Arne Bahlo <hallo@arne.me>
2014-04-14 12:45:16 +0200

    Add z-buffer

--- semester4/teil_1.cpp
+++ semester4/teil_1.cpp
@@ -16,57 +16,82 @@
 
 void Init()
 {
-   // Hier finden jene Aktionen statt, die zum Programmstart einmalig
-   // durchgeführt werden müssen
+  // Hier finden jene Aktionen statt, die zum Programmstart einmalig
+  // durchgeführt werden müssen
+  glEnable(GL_DEPTH_TEST);
+  glClearDepth(1.0);
 }
 
 void RenderScene() //Zeichenfunktion
 {
-   // Hier befindet sich der Code der in jedem Frame ausgefuehrt werden muss
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   glClearColor( 2.5, 0.5, 0.0, 1.0);
+  // Hier befindet sich der Code der in jedem Frame ausgefuehrt werden muss
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClearColor( 2.5, 0.5, 0.0, 1.0);
 
-   glLoadIdentity ();   // Aktuelle Model-/View-Transformations-Matrix zuruecksetzen
-   glBegin( GL_POLYGON );
-     glColor4f( 0.0, 0.0, 1.0, 1.0);
-     glVertex3f( -0.5, -0.5, -0.5 );
-     glVertex3f(  0.5, -0.5, -0.5 );
-     glVertex3f(  0.5,  0.5, -0.5 );
-     glVertex3f( -0.5,  0.5, -0.5 );
-   glEnd();
-   glFlush(); //Buffer leeren
+  glLoadIdentity ();   // Aktuelle Model-/View-Transformations-Matrix zuruecksetzen
+
+  // Draw blue square
+  glBegin( GL_POLYGON );
+    glColor4f( 0.0, 0.0, 1.0, 1.0);
+    glVertex3f( -0.5, -0.5, -0.5 );
+    glVertex3f(  0.5, -0.5, -0.5 );
+    glVertex3f(  0.5,  0.5, -0.5 );
+    glVertex3f( -0.5,  0.5, -0.5 );
+  glEnd();
+
+  // Draw orange square
+  glBegin( GL_POLYGON );
+    glColor4f( 0., 1., 0., 1.);
+    glVertex3f( -0.5, -0.5, -1. );
+    glVertex3f(  0.5, -0.5, -1. );
+    glVertex3f(  0.5,  0.5, -1. );
+    glVertex3f( -0.5,  0.5, -1. );
+  glEnd();
+
+  glFlush(); //Buffer leeren
 }
 
 void Reshape(int width,int height)
 {
-   // Hier finden die Reaktionen auf eine Veränderung der Größe des
-   // Graphikfensters statt
+  // Hier finden die Reaktionen auf eine Veränderung der Größe des
+  // Graphikfensters statt
+
+  // Matrix fu¿r Transformation: Frustum->viewport
+  glMatrixMode(GL_PROJECTION);
+  // Aktuelle Transformations-Matrix zuruecksetzen
+  glLoadIdentity ();
+  // Viewport definieren
+  glViewport(0,0,width,height);
+  // Frustum definieren (siehe unten)
+  glOrtho( -1., 1., -1., 1., 0.3, 1.3);
+  // Matrix fu¿r Modellierung/Viewing
+  glMatrixMode(GL_MODELVIEW);
 }
 
 void Animate (int value)
 {
-   // Hier werden Berechnungen durchgeführt, die zu einer Animation der Szene
-   // erforderlich sind. Dieser Prozess läuft im Hintergrund und wird alle
-   // 1000 msec aufgerufen. Der Parameter "value" wird einfach nur um eins
-   // inkrementiert und dem Callback wieder uebergeben.
-   std::cout << "value=" << value << std::endl;
-   // RenderScene aufrufen
-   glutPostRedisplay();
-   // Timer wieder registrieren; Animate wird so nach 100 msec mit value+=1 aufgerufen
-   glutTimerFunc(100, Animate, ++value);
+  // Hier werden Berechnungen durchgeführt, die zu einer Animation der Szene
+  // erforderlich sind. Dieser Prozess läuft im Hintergrund und wird alle
+  // 1000 msec aufgerufen. Der Parameter "value" wird einfach nur um eins
+  // inkrementiert und dem Callback wieder uebergeben.
+  std::cout << "value=" << value << std::endl;
+  // RenderScene aufrufen
+  glutPostRedisplay();
+  // Timer wieder registrieren; Animate wird so nach 100 msec mit value+=1 aufgerufen
+  glutTimerFunc(100, Animate, ++value);
 }
 
 int main(int argc, char **argv)
 {
-   glutInit( &argc, argv );                // GLUT initialisieren
-   glutInitDisplayMode( GLUT_SINGLE | GLUT_RGB );
-   glutInitWindowSize( 600, 600 );         // Fenster-Konfiguration
-   glutCreateWindow( "Name_1; Name_2" );   // Fenster-Erzeugung
-   glutDisplayFunc( RenderScene );         // Zeichenfunktion bekannt machen
-   glutReshapeFunc( Reshape );
-   // TimerCallback registrieren; wird nach 10 msec aufgerufen mit Parameter 0
-   glutTimerFunc( 10, Animate, 0);
-   Init();
-   glutMainLoop();
-   return 0;
+  glutInit( &argc, argv );                // GLUT initialisieren
+  glutInitDisplayMode( GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH );
+  glutInitWindowSize( 600, 600 );         // Fenster-Konfiguration
+  glutCreateWindow( "Arne Bahlo; Johannes Wahl" );   // Fenster-Erzeugung
+  glutDisplayFunc( RenderScene );         // Zeichenfunktion bekannt machen
+  glutReshapeFunc( Reshape );
+  // TimerCallback registrieren; wird nach 10 msec aufgerufen mit Parameter 0
+  glutTimerFunc( 10, Animate, 0);
+  Init();
+  glutMainLoop();
+  return 0;
 }
--- semester4/teil_1.md
+++ semester4/teil_1.md
@@ -6,4 +6,6 @@
 4. Vor der glLoadIdentity()
 5. Logischerweise muss zuerst glClear() aufgerufen werden, bevor glClearColor() aufgerufen wird. Sonst würde die gerade gesetzte Hintergrundfarbe direkt wieder gelöscht werden.
 6. Die verschiedenen Frames würden sich überlagern
+7. Nur die grüne
+8. Die Fläche, die zuletzt gezeichnet wurde, überlagert alle anderen
 
