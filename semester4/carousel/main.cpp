2aa1bcdfc6fbacde871894da6476c3cf01a42bb7
Arne Bahlo <hallo@arne.me>
2014-05-05 14:23:36 +0200

    Add main.cpp

---
+++ semester4/carousel/main.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <iostream>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
+#include "stdafx.h"
+#include <gl\freeglut.h>
+#endif
+
+
+int main(int argc, char **argv) {
+  
+}
